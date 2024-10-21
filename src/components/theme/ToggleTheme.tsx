f4eddfb8087519b5fdcd3095f47f57753f4a72ea
Y1am0 <ymorfoscc@gmail.com>
2024-10-21 04:40:35 +0300

    hotfix

--- src/components/theme/ToggleTheme.tsx
+++ src/components/theme/ToggleTheme.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-import { useTheme } from './theme/ThemeContext'
+import { useTheme } from './ThemeContext'
 
 export default function ToggleTheme() {
   const { toggleTheme } = useTheme()
